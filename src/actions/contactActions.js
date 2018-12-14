02deaaa8342a23ddb12e94fc039cb2db1fae3d74
Akinde Oluwafemi <oluwafemiakinde#gmail.com>
2018-12-14 17:49:52 +0100

    Modify contact reducer

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,21 +1,34 @@
-import { GET_CONTACTS, ADD_CONTACT, DELETE_CONTACT } from './types';
+import { GET_CONTACTS, ADD_CONTACT, DELETE_CONTACT, GET_CONTACT, UPDATE_CONTACT } from './types';
+import axios from 'axios';
 
-export const getContacts = () => {
-    return {
-        type: GET_CONTACTS
-    }
+export const getContacts = () => async dispatch => {
+    const res = await axios.get('http://jsonplaceholder.typicode.com/users');
+    dispatch ({
+        type: GET_CONTACTS,
+        payload: res.data
+    });
 }
 
-export const deleteContact = id => {
-    return {
+export const getContact = (id) => async dispatch => {
+    const res = await axios.get(`http://jsonplaceholder.typicode.com/users/${id}`);
+    dispatch ({
+        type: GET_CONTACTS,
+        payload: res.data
+    });
+}
+
+export const deleteContact = id => async dispatch => {
+    await axios.get(`http://jsonplaceholder.typicode.com/users/${id}`);
+    dispatch ({
         type: DELETE_CONTACT,
         payload: id
-    }
+    })
 }
 
-export const addContact = contact => {
-    return {
+export const addContact = contact => async dispatch => {
+    const res = await axios.post(`http://jsonplaceholder.typicode.com/users`, contact)
+    dispatch ({
         type: ADD_CONTACT,
-        payload: contact
-    }
+        payload: res.data
+    })
 }
--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,35 +1,27 @@
 import {
     GET_CONTACTS,
     ADD_CONTACT,
-    DELETE_CONTACT
+    DELETE_CONTACT,
+    GET_CONTACT,
+    UPDATE_CONTACT
 } from '../actions/types';
 const initialState = {
-    contacts: [{
-            id: 1,
-            name: 'John Doe',
-            email: 'john@gmail.com',
-            phone: '555-555-5555'
-        },
-        {
-            id: 2,
-            name: 'Karen Williams',
-            email: 'karen@gmail.com',
-            phone: '444-444-4444'
-        },
-        {
-            id: 3,
-            name: 'Henry Johnson',
-            email: 'henry@gmail.com',
-            phone: '333-333-333'
-        }
-    ]
+    contacts: [],
+    contact: {}
 };
 
 export default function (state = initialState, action) {
     switch (action.type) {
         case GET_CONTACTS:
             return {
-                ...state
+                ...state,
+                contacts: action.payload
+            }
+            break;
+        case GET_CONTACT:
+            return {
+                ...state,
+                contact: action.payload
             }
             break;
         case DELETE_CONTACT:
